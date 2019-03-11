1ef344e2cca22f9947fc5dd4050235ae6bacc1b8
priyankrawat <30529816+priyankrawat@users.noreply.github.com>
2019-03-11 23:11:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,439 @@
+<?php include_once "insert.php"; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Free View Hotel</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="favicon.ico" >
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700" rel="stylesheet">
+
+
+
+
+
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 50%;
+  padding: 150px;
+}
+
+/* Clearfix (clear floats) */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+
+
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand page-scroll" href="#page-top">Free View Hotel</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about" class="page-scroll">About</a></li>
+        <li><a href="#restaurant-menu" class="page-scroll">Menu</a></li>
+        <li><a href="#portfolio" class="page-scroll">Gallery</a></li>
+        <li><a href="#team" class="page-scroll">Service</a></li>
+        <li><a href="#call-reservation" class="page-scroll">Contact</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+</nav>
+<!-- Header -->
+<header id="header">
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1>Free View Hotel</h1>
+            <p>Restaurant / Coffee / Pub</p>
+            <a href="#about" class="btn btn-custom btn-lg page-scroll">Discover Story</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- About Section -->
+<div id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6 ">
+        <div class="about-img"><img src="img/about.jpg" class="img-responsive" alt=""></div>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <h2>Our Restaurant</h2>
+          <hr>
+          <p class="promo-block-text" style="font-size:25px;">In the very heart of town, at level with Shimla’s elegant Mall and close to the Tourism Lift, Free View Hotel is Shimla’s most modern hotel.
+
+It successfully combines today’s amenities, service and comforts with the graceful ways of life that belong to an older era...</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Restaurant Menu Section -->
+
+
+
+
+
+
+
+
+
+
+
+
+<div id="restaurant-menu">
+  <div class="section-title text-center center">
+    <div class="overlay">
+      <h2>Menu</h2>
+      <hr>
+      <p class="promo-block-text" style="font-size:25px;">Place where charming people meet to celebrate life's simple stories!
+Free View Hotel is a cozy hideaway where delicious food, heavy laughter and good conversation come together in a delightful fusion with lingering aroma of freshly treated food ingredients.
+
+The fine dining restaurant, offers cuisines from all over the world, prepared by its renowned chefs to satisfy the taste buds of every guest.
+
+With lip-smacking multi-cuisine, ultra modern interiors and great service; Free View Hotel is bound to be your favorite!</p>
+    </div>
+  </div>
+</div>
+  
+
+
+<div class="row">
+  <div class="column">
+  <h2 class="menu-section-title">BREAKFAST & STARTERS
+</h2>
+          <hr>
+       <p class="promo-block-text" style="font-size:25px;" > -- Continental Breakfast :- Café complet, i.e., coffee or tea complemented(or completed) with rolls, butter, conserve.<br><br><br>
+-- Breakfast:- Petit déjeuner, the French term includes both Continental and English / American breakfasts.</p>
+
+  </div>
+  <div class="column">
+   
+  <h2 class="menu-section-title">Main Course
+</h2>
+          <hr>
+       <p class="promo-block-text" style="font-size:25px;" > -- Dinner:- Diner in French, is regarded as the principal meal, and affords an opportunity for artistry in menu composition. At dinner, guests come for relaxation, to enjoy food, dining rituals and the atmosphere. Customers appreciate a longer menu with a choice of more exotic or esteemed dishes.<br><br><br>
+-- Déjeuner or Lunch is a meal with a shorter menu and with fewer courses than a dinner menu, with emphasis on stews, roast joints, grilled meats, cold buffet and hot puddings.</p>
+
+ </div>
+ 
+</div>
+
+<!-- Portfolio Section -->
+<div id="portfolio">
+  <div class="section-title text-center center">
+    <div class="overlay">
+      <h2>Gallery</h2>
+      <hr>
+     
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="categories">
+        <ul class="cat">
+          <li>
+            <ol class="type">
+              <li><a href="#" data-filter="*" class="active">All</a></li>
+              <li><a href="#" data-filter=".breakfast">Breakfast</a></li>
+              <li><a href="#" data-filter=".lunch">Lunch</a></li>
+              <li><a href="#" data-filter=".dinner">Dinner</a></li>
+            </ol>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/01-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 dinner">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/02-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/02-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/03-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4</h4>
+              </div>
+              <img src="img/portfolio/03-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/04-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/04-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 dinner">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/05-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/05-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/06-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/06-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/07-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/07-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/08-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/08-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 dinner">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/09-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/09-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/10-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/10-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 lunch">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/11-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/11-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-4 breakfast">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/12-large.jpg" title="" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4></h4>
+              </div>
+              <img src="img/portfolio/12-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Team Section -->
+<div id="team" class="text-center">
+  <div class="overlay">
+    <div class="container">
+      <div class="col-md-10 col-md-offset-1 section-title">
+        <p class="promo-block-text" style="font-size:25px;" >The hotel offers exceptionally good services and amenities. A stay at Free View Hotel will make your journey completely enjoyable and memorable, making you long for your next visit here.</p>
+        <hr>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- Call Reservation Section -->
+<div id="call-reservation" class="text-center">
+  <div class="container">
+    <h2>Want to make a reservation? Call <strong>7838687877</strong></h2>
+  </div>
+</div>
+<!-- Contact Section -->
+
+<br><br>
+
+<div class="container" style="width:520px;">  
+                <form id="submit_form">  
+                     <label>Username</label>  
+                     <input type="text" name="username" id="username" class="form-control" />  
+                     <br /> 
+                     <label>User Email</label>  
+                     <input type="email" id="email" class="form-control" /> 
+                     <br />  
+                      <label>Mobile Number</label>  
+                     <input type="phone" id="phone" class="form-control" /> 
+                     <br /> 
+                     <label>Message</label>  
+                     <textarea name="message" id="message" class="form-control"></textarea>  
+                     <br />  
+
+                     <input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />  
+                     <span id="error_message" class="text-danger"></span>  
+                     <span id="success_message" class="text-success"></span>  
+                </form>  
+           </div>  
+
+
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-4">
+      <h3>Address</h3>
+      <div class="contact-item">
+        <p>hotel free view</p>
+        <p>Shimla,main road.</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Opening Hours</h3>
+      <div class="contact-item">
+        <p>Mon-Thurs: 10:00 AM - 11:00 PM</p>
+        <p>Fri-Sun: 11:00 AM - 02:00 AM</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Contact Info</h3>
+      <div class="contact-item">
+        <p>Phone: 7838687877</p>
+        <p>Email: info@freeviewhotel.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center copyrights">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="social">
+        <ul>
+          <li><a href="fb.com"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="twitter.com"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="gmail.com"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+      </div>
+      <p>&copy; 2019 Free View Hotel. All rights reserved. Designed by<a href="http://priyank.ueuo.com/">Priyank Rawat</a></p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
+
+
+
+
+<script>  
+ $(document).ready(function(){  
+      $('#submit').click(function(){  
+           var username = $('#username').val();  
+           var email = $('#email').val();
+           var phone = $('#phone').val();  
+           var message = $('#message').val();  
+           if(username == '' || email == '' || phone == '' || message == '')  
+           {  
+                $('#error_message').html("All Fields are required");  
+           }  
+           else  
+           {  
+                $('#error_message').html('');  
+                $.ajax({  
+                     url:"insert.php",  
+                     method:"POST",  
+                     data:{username:username, email:email, phone:phone, message:message},  
+                     success:function(data){  
+                          $("form").trigger("reset");  
+                          $('#success_message').fadeIn().html(data);  
+                          setTimeout(function(){  
+                               $('#success_message').fadeOut("Slow");  
+                          }, 2000);  
+                     }  
+                });  
+           }  
+      });  
+ });  
+ </script>  
